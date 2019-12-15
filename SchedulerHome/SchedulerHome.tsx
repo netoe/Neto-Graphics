9da08edad77b86d8a519299d457a9176b5137468
Yu Zhanbei <awesome.zhanbei@gmail.com>
2019-12-15 15:11:57 +0800

    [SchedulerHome|helpers/bridge*] Added #getNetoScheduleReceiptsManager().

--- SchedulerHome/SchedulerHome.tsx
+++ SchedulerHome/SchedulerHome.tsx
@@ -3,9 +3,11 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import {LayoutEmbeddedApp} from '../components/LayoutEmbeddedApp';
-import {doReportTheLostOfNetoBridge, getNetoDemoAndDevelopment} from '../helpers/bridge-neto-core';
+import {doReportTheLostOfNetoBridge, getNetoDemoAndDevelopment, getNetoScheduleReceiptsManager} from '../helpers/bridge-neto-core';
 import {R} from './resources';
 import {useStyles} from './styles';
+
+const ScheduleReceiptsManager = getNetoScheduleReceiptsManager();
 
 let title = R.title;
 
@@ -15,8 +17,7 @@ export const SchedulerHome = React.memo<IProps>(() => {
 	const cls = useStyles();
 	const [NetoDemoAndDevelopment] = React.useState(getNetoDemoAndDevelopment);
 
-	const ScheduleReceiptsManager: any = window['_NetoScheduleReceiptsManager'];
-	const receipts: any = ScheduleReceiptsManager ? ScheduleReceiptsManager.receipts : undefined;
+	const receipts: any = ScheduleReceiptsManager ? ScheduleReceiptsManager.getScheduleReceipts('all') : undefined;
 
 	console.log('found receipts:', receipts);
 
--- helpers/bridge-neto-core.ts
+++ helpers/bridge-neto-core.ts
@@ -5,6 +5,7 @@ import {getNetoCoreBridge} from '../../bridges/BridgeNetoCore';
 const bridge = getNetoCoreBridge();
 export const getNetoBackgroundService = () => bridge ? bridge.BackgroundService : undefined;
 export const getNetoDemoAndDevelopment = () => bridge ? bridge.DemoAndDevelopment : undefined;
+export const getNetoScheduleReceiptsManager = () => bridge ? bridge.ScheduleReceiptsManager : undefined;
 export const doReportTheLostOfNetoBridge = (conf: { alert?: boolean } = {}) => {
 	const message = 'The expected neto bridge is not found, please checkout your configures!';
 	console.error(message);
