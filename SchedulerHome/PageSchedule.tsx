2ccabc4282b0911ab62898883ffc2ccf129a6466
Yu Zhanbei <awesome.zhanbei@gmail.com>
2019-12-19 21:59:41 +0800

    [SchedulerHome/PageSchedule] Added the simple table of actions.

--- SchedulerHome/PageSchedule.tsx
+++ SchedulerHome/PageSchedule.tsx
@@ -4,15 +4,26 @@ import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import {Schedule} from '../../core/scheduler/schedule';
+import {IAction} from '../../core/scheduler/typed/actions';
 import {ViewLabelsAndValues} from '../mui-lib/views/ViewLabelsAndValues';
+import {getViewSelectableTable, newTableColumn} from '../mui-lib/widgets/TableSelectableRows';
 import {useStyles} from './PageSchedule.styles';
 
 interface IProps {
 	schedule: Schedule;
 }
 
+const TableScheduleActions = getViewSelectableTable<IAction<object>>();
+
 export const PageSchedule = React.memo<IProps>(({schedule}) => {
 	const cls = useStyles();
+
+	const [selectedAction, setSelectedAction] = React.useState(undefined as IAction | undefined);
+
+	const onRowSelected = (actionId: any, action: IAction) => {
+		console.log('selected:', actionId, action);
+		setSelectedAction(action);
+	};
 
 	const renderPageBody = () => (
 		<Paper className={cls.ctnPaper}>
@@ -22,6 +33,10 @@ export const PageSchedule = React.memo<IProps>(({schedule}) => {
 			<div className={cls.ctnContent}>
 				{renderScheduleDetails()}
 				{renderLatestScheduleReceipt()}
+			</div>
+
+			<div>
+				{renderScheduleActions()}
 			</div>
 
 		</Paper>
@@ -38,6 +53,23 @@ export const PageSchedule = React.memo<IProps>(({schedule}) => {
 
 	const renderLatestScheduleReceipt = () => (
 		<div className={cls.ctnContentSection}/>
+	);
+
+	// TableScheduleActions.tsx
+	const renderScheduleActions = () => (
+		<TableScheduleActions
+			keyEntryId={'_id'}
+			onSelectEntry={onRowSelected}
+			selectedEntryId={selectedAction?._id}
+			placeholder={'Fine'}
+			entries={schedule.actions}
+			columns={[
+				// newTableColumn('_id', 'ID', 'center'),
+				newTableColumn('name', 'Name', 'center'),
+				newTableColumn('key', 'Key', 'center'),
+				newTableColumn('name', 'Description', 'left'),
+			]}
+		/>
 	);
 
 	return renderPageBody();
