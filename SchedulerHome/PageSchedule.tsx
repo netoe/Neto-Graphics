8fc9c238ab67b0f0171d28155ec97d8827ddad1d
Yu Zhanbei <awesome.zhanbei@gmail.com>
2019-12-19 23:02:45 +0800

    [SchedulerHome/PageSchedule] Integrated schedule receipts the rushy way.

--- SchedulerHome/PageSchedule.tsx
+++ SchedulerHome/PageSchedule.tsx
@@ -5,6 +5,8 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import {Schedule} from '../../core/scheduler/schedule';
 import {IAction} from '../../core/scheduler/typed/actions';
+import {IScheduleReceipt} from '../../core/scheduler/typed/receipts';
+import {getNetoScheduleReceiptsManager} from '../helpers/bridge-neto-core';
 import {ViewLabelsAndValues} from '../mui-lib/views/ViewLabelsAndValues';
 import {getViewSelectableTable, newTableColumn} from '../mui-lib/widgets/TableSelectableRows';
 import {useStyles} from './PageSchedule.styles';
@@ -14,15 +16,34 @@ interface IProps {
 }
 
 const TableScheduleActions = getViewSelectableTable<IAction<object>>();
+const TableScheduleReceipts = getViewSelectableTable<IScheduleReceipt>();
+const manager = getNetoScheduleReceiptsManager();
 
 export const PageSchedule = React.memo<IProps>(({schedule}) => {
 	const cls = useStyles();
 
+	const [receipts, setReceipts] = React.useState(undefined as IScheduleReceipt[] | undefined);
 	const [selectedAction, setSelectedAction] = React.useState(undefined as IAction | undefined);
+	const [selectedReceipt, setSelectedReceipt] = React.useState(undefined as IScheduleReceipt | undefined);
+
+	React.useEffect(() => {
+		setReceipts(undefined);
+		manager?.getScheduleReceipts(schedule.name).then(receipts => {
+			console.log('Get the receipts:', receipts);
+			setReceipts(receipts);
+		}).catch(ex => {
+			console.error('failed to get the receipts:', ex);
+		});
+	}, [schedule]);
 
 	const onRowSelected = (actionId: any, action: IAction) => {
 		console.log('selected:', actionId, action);
 		setSelectedAction(action);
+	};
+
+	const onReceiptSelected = (receiptId: any, receipt: IScheduleReceipt) => {
+		console.log('selected:', receiptId, receipt);
+		setSelectedReceipt(receipt);
 	};
 
 	const renderPageBody = () => (
@@ -36,9 +57,14 @@ export const PageSchedule = React.memo<IProps>(({schedule}) => {
 			</div>
 
 			<div>
+				<h2>Schedule Actions</h2>
 				{renderScheduleActions()}
 			</div>
 
+			<div>
+				<h2>Schedule Receipts</h2>
+				{renderScheduleReceipts()}
+			</div>
 		</Paper>
 	);
 
@@ -68,6 +94,25 @@ export const PageSchedule = React.memo<IProps>(({schedule}) => {
 				newTableColumn('name', 'Name', 'center'),
 				newTableColumn('key', 'Key', 'center'),
 				newTableColumn('name', 'Description', 'left'),
+			]}
+		/>
+	);
+
+	// TableScheduleReceipts.tsx
+	const renderScheduleReceipts = () => (
+		<TableScheduleReceipts
+			keyEntryId={'_id'}
+			onSelectEntry={onReceiptSelected}
+			selectedEntryId={selectedReceipt?._id}
+			placeholder={'Fine'}
+			entries={receipts}
+			columns={[
+				// newTableColumn('_id', 'ID', 'center'),
+				newTableColumn('_id', 'ID', 'center'),
+				newTableColumn('taskId', 'Task', 'center'),
+				newTableColumn('code', 'Code', 'center'),
+				newTableColumn('duration', 'Duration', 'center', undefined, undefined, (entry, value) => (value / 1000).toFixed(2) + 's'),
+				newTableColumn('time', 'Time', 'center', undefined, undefined, (entry, value, index) => new Date(value).toLocaleString()),
 			]}
 		/>
 	);
